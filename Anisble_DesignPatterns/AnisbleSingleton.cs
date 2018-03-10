251c7ee85a2f137772e4b59ec3720a89eb1ecbce
Karthik Elangovan <karthik.ellangovan@gmail.com>
2018-03-10 22:02:29 +1100

    checkin
    checkin

---
+++ Anisble_DesignPatterns/AnisbleSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anisble_DesignPatterns
+{
+   public class AnisbleSingleton 
+    {
+        private static readonly AnisbleSingleton singleInstance = new AnisbleSingleton();
+        public string currentText = "Initial Value";
+        private AnisbleSingleton()
+        {}
+        static AnisbleSingleton()
+        {
+            //readonly object are initialised in static contructor 
+        }
+        public static AnisbleSingleton SingleInstance
+        {
+            get
+            {
+                return singleInstance;
+            }
+        }
+       
+        public  void Print(string text)
+        {
+            currentText = text;
+            Console.WriteLine(text);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(currentText);
+        }
+    }
+
+}
---
+++ Anisble_DesignPatterns/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anisble_DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AnisbleSingleton.SingleInstance.currentText = "From Prg";
+            AnisbleSingleton.SingleInstance.Print();
+            Console.ReadLine();
+
+
+            AnisbleSingleton.SingleInstance.Print();
+            Console.ReadLine();
+
+            AnisbleSingleton.SingleInstance.Print("First");
+            Console.ReadLine();
+
+            AnisbleSingleton.SingleInstance.Print("Second");
+            Console.ReadLine();
+
+            AnisbleSingleton.SingleInstance.Print();
+            Console.ReadLine();
+
+             
+    }
+    }
+}
